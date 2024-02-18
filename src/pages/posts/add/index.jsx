8f3797852c098cad778552a4832c19c7539c6e6b
Filonych <109238905+Filonych@users.noreply.github.com>
2024-02-18 17:08:23 +0000

    add routing

---
+++ src/pages/posts/add/index.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const AddPost = () => <div>Это страничка добавления поста</div>
---
+++ src/pages/posts/edit/index.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const EditPost = () => <div>Это страничка редактирования поста</div>
