4ff0cafb609d40974b2c45c27535c4fbb962d6d0
Filonych <109238905+Filonych@users.noreply.github.com>
2024-02-25 14:01:33 +0000

    .

--- src/pages/registration/index.jsx
+++ src/pages/registration/index.jsx
@@ -7,9 +7,9 @@ import { Input } from '../../components/ui/Input'
 import { useNavigate } from "react-router-dom";
 
 export const RegistrationPage = () => {
+    const disabled = !formValues.email || !formValues.password
     const navigate = useNavigate()
     const [formValues, setFormValues] = useState({name: '', surname: '', email: '', password: ''})
-    const disabled = !formValues.email || !formValues.password
 
 
     const onSubmit = (e) => {
